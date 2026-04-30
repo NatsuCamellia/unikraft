bea08b49ae0c0dace15a34eaeb3c61e739cb3b02
NatsuCamellia <natsucamellia@gmail.com>
2026-04-30 14:55:22 +0000

    lib/ukrandom: Ignore ENODEV error
    Currently, if our host CPU doesn't support hardware RNG, the RNG driver
    would fail to initialize. Some apps like iperf3 still calls
    uk_random_fill_buffer even though the driver is not initialized. This
    change ignore ENODEV error now and leave the buffer unmodified.

--- lib/ukrandom/random.c
+++ lib/ukrandom/random.c
@@ -66,7 +66,10 @@ int __check_result uk_random_fill_buffer(void *buf, size_t buflen)
 	int rc;
 
 	if (!driver)
-		return -ENODEV;
+		// we did't have the device initialized, so we keep the buffer
+		// unmodified and pretend that we have filled some random
+		// numbers into the buffer.
+		return 0;
 
 	step = sizeof(__u32);
 	remain = buflen % step;
